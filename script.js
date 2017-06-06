e4fbd2e35983213391de5a7c35b2124b881a34a5
John Pfingst <jpfingst@outlook.com>
2017-06-06 22:51:00 +0000

    Done.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
   //   // inserting the response
   //   $("#sentences").html(response);
   // });
-  // 
+  //
   $.get("this_doesnt_exist.html", function(data) {
     // This will not be called because the .html file request doesn't exist
     doSomethingGood();
@@ -18,11 +18,11 @@ $(document).ready(function(){
 
 
   var url = "https://api.github.com/repos/rails/rails/commits?sha=82885325e04d78fb7ec608a4670164d842d23078";
- 
+
 // $.get(url)
 //   .done(function(data) {
 //     console.log("Done");
 //     console.log(data);
 //   });
-//   
+//
 });
